df3b4d7a99c68efec249a84f28f3e70de2b38ae6
DemuraAIdev <cyberaioff@gmail.com>
2024-05-31 22:35:15 +0700

    chore: Update dependencies and add console logging

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const config = {
+  port: 7053,
+  database:
+    "mysql://u16_TlhcsGvjSv:RHtxCxu.M7XWUEvE%2BIK!oYy%40@103.127.132.106:3306/s16_warn",
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-
+require("./lib/console");
 const app = express();
 const httpServer = createServer(app);
+const config = require("./config/config");
+const Keyv = require("keyv");
 const io = new Server(httpServer, {
   /* options */
 });
 
-httpServer.listen(7053);
-console.log("Server started");
+const db = new Keyv(config.database, { namespace: "warn" }).on("error", (err) =>
+  console.error("Keyv connection error:", err)
+);
+httpServer.listen(config.port);
+console.info("Server started at port " + config.port + ".");
 
 io.on("connection", (socket) => {
-  console.log(socket.id); // the id of the socket
-  socket.on("hello", (arg) => {
-    console.log(arg);
-    socket.emit("hello", "Welcome to DCORE, " + socket.id);
+  console.info("A Clinet connected: " + socket.id);
+  socket.on("notice", (arg) => {
+    console.info(socket.id + " said: " + arg);
+    socket.emit("notice", "Welcome to DCORE, " + socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.warn("A Clinet disconnected: " + socket.id);
+  });
+
+  // socket on ping
+  socket.on("ping", (cb) => {
+    console.info("pinged from" + socket.id);
+    cb({
+      status: "ok",
+    });
+  });
+
+  socket.on("db", async (query, callback) => {
+    console.info("db event from: " + socket.id);
+    const value = await db.get(query.key);
+    console.info("value: " + value);
+    callback({
+      status: "ok",
+      value: value,
+    });
+  });
+
+  socket.on("ready", (arg) => {
+    console.info("=====================================");
+    console.info("Ready Event from: " + socket.id);
+    console.info("Total Members: " + arg.totalMembers);
+    console.info("Prototype: " + arg.prototype);
+    console.info("=====================================");
   });
 });
 
+//
+
 io.engine.on("connection_error", (err) => {
-  console.log(err.req); // the request object
-  console.log(err.code); // the error code, for example 1
-  console.log(err.message); // the error message, for example "Session ID unknown"
-  console.log(err.context); // some additional error context
+  console.error(err.req); // the request object
+  console.error(err.code); // the error code, for example 1
+  console.error(err.message); // the error message, for example "Session ID unknown"
+  console.error(err.context); // some additional error context
 });
